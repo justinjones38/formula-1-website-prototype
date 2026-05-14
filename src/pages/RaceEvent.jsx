8f4ac9a5eb3bcf9482d862aee41ca5806e8faee4
Justin <justinjones38@myyahoo.com>
2026-05-14 08:44:10 -0500

    Added weekend events as link in calendar page

--- src/pages/RaceEvent.jsx
+++ src/pages/RaceEvent.jsx
@@ -1,13 +1,24 @@
 import { useOutletContext, useParams } from "react-router-dom";
+import ScheduleCard from "../components/cards/ScheduleCard";
+import styles from "./RaceEvent.module.css";
 
 export default function RaceCalendar() {
-  const {id} = useParams();
-  const {data} = useOutletContext();
-  console.log(data);
+  const { id } = useParams();
+  const { data } = useOutletContext();
   // Temporary check to prevent error.
   if (!data || Object.keys(data).length === 0) {
     return;
   }
 
-  return <h1>Hello World</h1>;
+  const { calendar } = data;
+  
+  const event = calendar.Races.find(event => event.Circuit.circuitId === id);
+  console.log(event);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Round {event.round} - {event.raceName}</h1>
+      <ScheduleCard event={event} />
+    </div>
+  );
 }
